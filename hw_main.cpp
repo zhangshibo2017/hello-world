4858256d17b247c6f45e968b4071849c982ffcc9
zhangshibo <zhangshibo@alibaba-inc.com>
2017-09-22 10:10:49 +0800

    to experiment friend + derived class(sub-class) mechanism

--- hw_main.cpp
+++ hw_main.cpp
@@ -1,8 +1,46 @@
 #include <cstdio>
+
 //should I use Coding standard ?
 //yes , I should
-// 
-int main(int argc, const char *[] agrv){
-	std::print("hello world!\n");
+// abc 
+void a (){
+	return ;
+}
+
+class Foo
+{
+	protected:
+	int a_;
+	int b_;
+
+};
+
+class Bar;
+
+class Foo1 : public Foo {
+	public:
+	friend class Bar;
+	protected:
+	int c_;
+};
+
+class Bar{
+public:	
+	Bar(){
+	
+		foo1_.c_ = 1;
+		foo1_.a_ = 2;
+		foo1_.b_ = 3;
+
+		std::printf("%d,%d,%d\n",foo1_.a_,foo1_.b_,foo1_.c_);
+	}
+private:
+	Foo1 foo1_;
+};
+
+int main(int argc, const char**  agrv)
+{
+	Bar bar;
+	std::printf("hello world!\n");
 	return 0;
 }
